49d3fcfd850c6af0c679d900bb3c0297fbeb4e30
Ondrej Certik <ondrej@certik.cz>
2010-03-10 18:52:28 -0800

    Tests for unsymmetric matrix added

--- tests/solvers/main.cpp
+++ tests/solvers/main.cpp
@@ -101,6 +101,49 @@ void test_solver3()
     _assert(fabs(res[3] - 0.2) < EPS);
 }
 
+void test_solver4()
+{
+    CooMatrix A(5);
+    A.add(0, 0, 2);
+    A.add(0, 1, 3);
+    A.add(1, 0, 3);
+    A.add(1, 2, 4);
+    A.add(1, 4, 6);
+    A.add(2, 1, -1);
+    A.add(2, 2, -3);
+    A.add(2, 3, 2);
+    A.add(3, 2, 1);
+    A.add(4, 1, 4);
+    A.add(4, 2, 2);
+    A.add(4, 4, 1);
+
+    double res[5] = {8., 45., -3., 3., 19.};
+    solve_linear_system_numpy(&A, res);
+    _assert(fabs(res[0] - 1.) < EPS);
+    _assert(fabs(res[1] - 2.) < EPS);
+    _assert(fabs(res[2] - 3.) < EPS);
+    _assert(fabs(res[3] - 4.) < EPS);
+    _assert(fabs(res[4] - 5.) < EPS);
+
+    /*
+    res = {8., 45., -3., 3., 19.};
+    solve_linear_system_scipy_umfpack(&A, res);
+    _assert(fabs(res[0] - 1.) < EPS);
+    _assert(fabs(res[1] - 2.) < EPS);
+    _assert(fabs(res[2] - 3.) < EPS);
+    _assert(fabs(res[3] - 4.) < EPS);
+    _assert(fabs(res[4] - 5.) < EPS);
+
+    res = {8., 45., -3., 3., 19.};
+    solve_linear_system_scipy_cg(&A, res);
+    _assert(fabs(res[0] - 1.) < EPS);
+    _assert(fabs(res[1] - 2.) < EPS);
+    _assert(fabs(res[2] - 3.) < EPS);
+    _assert(fabs(res[3] - 4.) < EPS);
+    _assert(fabs(res[4] - 5.) < EPS);
+    */
+}
+
 int main(int argc, char* argv[])
 {
     try {
@@ -112,6 +155,7 @@ int main(int argc, char* argv[])
         test_solver1();
         test_solver2();
         test_solver3();
+        test_solver4();
 
         return ERROR_SUCCESS;
     } catch(std::exception const &ex) {
