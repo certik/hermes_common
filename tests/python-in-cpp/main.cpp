757ad10dd2c202ab2f657f0df6c36c80b1ac414f
Ondrej Certik <ondrej@certik.cz>
2010-03-20 16:15:32 -0700

    Prepare a test for namespaces

--- tests/python-in-cpp/main.cpp
+++ tests/python-in-cpp/main.cpp
@@ -13,6 +13,7 @@ void _assert(bool a)
     if (!a) throw std::runtime_error("Assertion failed.");
 }
 
+// Test the global python instance
 void test_basic1()
 {
     python->insert_object("i", c2py_int(5));
@@ -21,6 +22,7 @@ void test_basic1()
     _assert(i == 10);
 }
 
+// Test the local Python() instance
 void test_basic2()
 {
     Python *p = new Python();
@@ -31,6 +33,7 @@ void test_basic2()
     delete p;
 }
 
+// Test initialization/destruction of two Python() instances
 void test_basic3()
 {
     Python *p1 = new Python();
@@ -48,6 +51,24 @@ void test_basic3()
     delete p2;
 }
 
+// Test that each Python() instance has it's own namespace
+void test_basic4()
+{
+    Python *p1 = new Python();
+    Python *p2 = new Python();
+    p1->insert_object("i", c2py_int(5));
+    p2->insert_object("i", c2py_int(6));
+    p1->eval("i = i*2");
+    p2->eval("i = i*2");
+    int i1 = py2c_int(p1->get_object("i"));
+    int i2 = py2c_int(p2->get_object("i"));
+    // Fails so far:
+    //_assert(i1 == 10);
+    //_assert(i2 == 12);
+    delete p1;
+    delete p2;
+}
+
 int main(int argc, char* argv[])
 {
     try {
@@ -56,6 +77,7 @@ int main(int argc, char* argv[])
         test_basic1();
         test_basic2();
         test_basic3();
+        test_basic4();
 
         delete python;
 
