3abe7a8c90bb8c98b76ee019827a0884d661f905
Ondrej Certik <ondrej@certik.cz>
2010-03-22 15:20:10 -0700

    Use the Python() class in solvers

--- python_solvers.cpp
+++ python_solvers.cpp
@@ -9,64 +9,64 @@
 
 void solve_linear_system_numpy(CooMatrix *mat, double *res)
 {
-    if (import__hermes_common())
-        throw std::runtime_error("hermes_common failed to import.");
     CSRMatrix M(mat);
-    insert_object("m", c2py_CSRMatrix(&M));
-    insert_object("rhs", c2numpy_double_inplace(res, mat->get_size()));
-    cmd("A = m.to_scipy_csr().todense()");
-    cmd("from numpy.linalg import solve");
-    cmd("x = solve(A, rhs)");
+    Python *p = new Python();
+    p->push("m", c2py_CSRMatrix(&M));
+    p->push("rhs", c2numpy_double_inplace(res, mat->get_size()));
+    p->exec("A = m.to_scipy_csr().todense()");
+    p->exec("from numpy.linalg import solve");
+    p->exec("x = solve(A, rhs)");
     double *x;
     int n;
-    numpy2c_double_inplace(get_object("x"), &x, &n);
+    numpy2c_double_inplace(p->pull("x"), &x, &n);
     memcpy(res, x, n*sizeof(double));
+    delete p;
 }
 
 void solve_linear_system_scipy_umfpack(CooMatrix *mat, double *res)
 {
-    if (import__hermes_common())
-        throw std::runtime_error("hermes_common failed to import.");
     CSCMatrix M(mat);
-    insert_object("m", c2py_CSCMatrix(&M));
-    insert_object("rhs", c2numpy_double_inplace(res, mat->get_size()));
-    cmd("A = m.to_scipy_csc()");
-    cmd("from scipy.sparse.linalg import spsolve");
-    cmd("x = spsolve(A, rhs)");
+    Python *p = new Python();
+    p->push("m", c2py_CSCMatrix(&M));
+    p->push("rhs", c2numpy_double_inplace(res, mat->get_size()));
+    p->exec("A = m.to_scipy_csc()");
+    p->exec("from scipy.sparse.linalg import spsolve");
+    p->exec("x = spsolve(A, rhs)");
     double *x;
     int n;
-    numpy2c_double_inplace(get_object("x"), &x, &n);
+    numpy2c_double_inplace(p->pull("x"), &x, &n);
     memcpy(res, x, n*sizeof(double));
+    delete p;
 }
 
 void solve_linear_system_scipy_cg(CooMatrix *mat, double *res)
 {
-    if (import__hermes_common())
-        throw std::runtime_error("hermes_common failed to import.");
     CSRMatrix M(mat);
-    insert_object("m", c2py_CSRMatrix(&M));
-    insert_object("rhs", c2numpy_double_inplace(res, mat->get_size()));
-    cmd("A = m.to_scipy_csr()");
-    cmd("from scipy.sparse.linalg import cg");
-    cmd("x, res = cg(A, rhs)");
+    Python *p = new Python();
+    p->push("m", c2py_CSRMatrix(&M));
+    p->push("rhs", c2numpy_double_inplace(res, mat->get_size()));
+    p->exec("A = m.to_scipy_csr()");
+    p->exec("from scipy.sparse.linalg import cg");
+    p->exec("x, res = cg(A, rhs)");
     double *x;
     int n;
-    numpy2c_double_inplace(get_object("x"), &x, &n);
+    numpy2c_double_inplace(p->pull("x"), &x, &n);
     memcpy(res, x, n*sizeof(double));
+    delete p;
 }
 
 void solve_linear_system_scipy_gmres(CooMatrix *mat, double *res)
 {
-    if (import__hermes_common())
-        throw std::runtime_error("hermes_common failed to import.");
     CSRMatrix M(mat);
-    insert_object("m", c2py_CSRMatrix(&M));
-    insert_object("rhs", c2numpy_double_inplace(res, mat->get_size()));
-    cmd("A = m.to_scipy_csr()");
-    cmd("from scipy.sparse.linalg import gmres");
-    cmd("x, res = gmres(A, rhs)");
+    Python *p = new Python();
+    p->push("m", c2py_CSRMatrix(&M));
+    p->push("rhs", c2numpy_double_inplace(res, mat->get_size()));
+    p->exec("A = m.to_scipy_csr()");
+    p->exec("from scipy.sparse.linalg import gmres");
+    p->exec("x, res = gmres(A, rhs)");
     double *x;
     int n;
-    numpy2c_double_inplace(get_object("x"), &x, &n);
+    numpy2c_double_inplace(p->pull("x"), &x, &n);
     memcpy(res, x, n*sizeof(double));
+    delete p;
 }
