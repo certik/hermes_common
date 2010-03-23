bda4c317120cd8666b9926b0c5f3bf10756ca6cd
Ondrej Certik <ondrej@certik.cz>
2010-03-22 18:38:38 -0700

    Check that __del__() method is executed

--- tests/leaks/main.cpp
+++ tests/leaks/main.cpp
@@ -2,6 +2,11 @@
 #include <stdexcept>
 
 #include "python_api.h"
+#define __PYX_HAVE_RT_ImportModule
+#define __PYX_HAVE_RT_ImportFunction
+#include "my_api2_api.h"
+
+#include "cpp_api.h"
 
 #define ERROR_SUCCESS                               0
 #define ERROR_FAILURE                              -1
@@ -26,28 +31,47 @@ void use_big_numpy_array1(int size)
 
 void test_leaks1()
 {
-    // If the matrices are not deallocated, this should take ~760GB:
+    // If the matrices are not deallocated, this should take ~19GB:
     for (int i=0; i<5; i++)
-        // Roughly 0.76GB:
+        // Roughly 3.8GB:
         use_big_numpy_array1(5*pow(10,8));
 }
+
+class MyCallback: public CppCallback {
+    public:
+        MyCallback(): CppCallback() {
+            this->called = 0;
+        }
+        virtual void event(const char *msg) {
+            printf("MyCallback event(%s) got called.\n", msg);
+            this->called = 1;
+        }
+        int called;
+};
 
 void test_dealloc()
 {
     Python *p = new Python();
+    if (import_my_api2())
+        throw std::runtime_error("my_api failed to import.");
+    MyCallback *c = new MyCallback();
+    p->push("c", c2py_CppCallback(c));
+    _assert(c->called == 0);
     p->exec("from test_dealloc import A");
-    p->exec("a = A()");
+    p->exec("a = A(c)");
     p->exec("print 'A() was created'");
+    _assert(c->called == 0);
     delete p;
+    // This tests that the __del__ method of A() was callled:
+    _assert(c->called == 1);
 }
-
-// Test gettting the data from the array back
-// ...
 
 int main(int argc, char* argv[])
 {
     try {
-        test_leaks1();
+        // Enable this by hand if you want to test with big matrices (might
+        // consume all your memory if something goes wrong):
+        //test_leaks1();
         test_dealloc();
 
         return ERROR_SUCCESS;
