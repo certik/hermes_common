75aa929f40bb4483d4f3d49001f9f934a0b34362
Ondrej Certik <ondrej@certik.cz>
2010-06-02 15:52:36 -0700

    Remove unnecessary include Python.h

--- stdcython.h
+++ stdcython.h
@@ -1,12 +1,6 @@
 #ifndef STDCYTHON_H
 #define STDCYTHON_H
 
-// To avoid compilation warnings:
-#undef _XOPEN_SOURCE
-#undef _POSIX_C_SOURCE
-
-#include "Python.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
